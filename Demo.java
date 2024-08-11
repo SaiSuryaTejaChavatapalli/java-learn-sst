c0862c0d22c88a55ee7e556a9a65fedba2aaf460
Sai Surya Teja <saisuryateja3@gmail.com>
2024-08-11 15:35:32 +0530

    Methods

--- Demo.java
+++ Demo.java
@@ -7,6 +7,16 @@ class Calculator{
     }
 }
 
+class Computer{
+    public void playMusic(){
+        System.out.println("Playing music");
+    }
+    public String getMePen(int cost){
+        if(cost>=10) return "Pen";
+        return "Nothing";
+    }
+}
+
 public class Demo {
     public static void main(String a[]){
         int num1=4;
@@ -15,5 +25,12 @@ public class Demo {
         Calculator calc=new Calculator();
         int result=calc.add(num1,num2);
         System.out.println(result);
+
+        Computer com=new Computer();
+        com.playMusic();
+        String pen=com.getMePen(2);
+        System.out.println(pen);
+        String pen1=com.getMePen(12);
+        System.out.println(pen1);
     }
 }

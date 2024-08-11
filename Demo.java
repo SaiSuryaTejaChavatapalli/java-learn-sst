350e9ef04f697d7f27d030fccc40aec57c087532
Sai Surya Teja <saisuryateja3@gmail.com>
2024-08-11 22:51:09 +0530

    Jagged Array

--- Demo.java
+++ Demo.java
@@ -27,5 +27,23 @@ public class Demo {
         }
         System.out.println();
       }
+
+      //Jagged Array
+      int jaggedArray[][]=new int[3][];
+      jaggedArray[0]=new int[4];
+      jaggedArray[1]=new int[2];
+      jaggedArray[2]=new int[5];
+
+      for(int i=0;i<jaggedArray.length;i++){
+        for(int j=0;j<jaggedArray[i].length;j++){
+            jaggedArray[i][j]=(int)(Math.random()*10);
+        }
+      }
+      for(int i[]:jaggedArray){
+        for(int j:i){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+      }
     }
 }

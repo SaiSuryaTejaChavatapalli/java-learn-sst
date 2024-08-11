af5a8b2a581e6de9f234e4f234939926418fb372
Sai Surya Teja <saisuryateja3@gmail.com>
2024-08-11 15:53:59 +0530

    Method overloading

--- Demo.java
+++ Demo.java
@@ -1,21 +1,24 @@
 class Calculator{
-    int x;
     public int add(int num1,int num2){
-
-        int r=num1+num2;
-        return r;
+        return num1+num2;
+    }
+    public int add(int n1,int n2,int n3){
+        return n1+n2+n3;
+    }
+    public double add(double n1, int n2){
+        return n1+n2;
     }
 }
 
-class Computer{
-    public void playMusic(){
-        System.out.println("Playing music");
-    }
-    public String getMePen(int cost){
-        if(cost>=10) return "Pen";
-        return "Nothing";
-    }
-}
+// class Computer{
+//     public void playMusic(){
+//         System.out.println("Playing music");
+//     }
+//     public String getMePen(int cost){
+//         if(cost>=10) return "Pen";
+//         return "Nothing";
+//     }
+// }
 
 public class Demo {
     public static void main(String a[]){
@@ -23,14 +26,20 @@ public class Demo {
         int num2=5;
         //System.out.println(num1+num2);
         Calculator calc=new Calculator();
-        int result=calc.add(num1,num2);
-        System.out.println(result);
+        int result1=calc.add(num1,num2);
+        System.out.println(result1);
 
-        Computer com=new Computer();
-        com.playMusic();
-        String pen=com.getMePen(2);
-        System.out.println(pen);
-        String pen1=com.getMePen(12);
-        System.out.println(pen1);
+        int result2=calc.add(1,2,3);
+        System.out.println(result2);
+
+        // int result3=calc.add(1.5,2);
+        // System.out.println(result3);
+
+        // Computer com=new Computer();
+        // com.playMusic();
+        // String pen=com.getMePen(2);
+        // System.out.println(pen);
+        // String pen1=com.getMePen(12);
+        // System.out.println(pen1);
     }
 }
